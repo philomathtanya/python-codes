e7d80f889194b5192880fca9bbabb1cedb753a2c
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2021-03-17 21:39:43 +0530

    Add files via upload

---
+++ hypo.py
@@ -0,0 +1,4 @@
+b=eval(input("enter base"))
+p=eval(input("enter perpendicular"))
+hypo=((b**2)+(p**2))**0.5
+print('%.2f'%hypo)
---
+++ peri and area.py
@@ -0,0 +1,7 @@
+l=eval(input("enter length"))
+b=eval(input("enter breadth"))
+area=l*b
+perimeter=2*(l+b)
+print(area)
+#perimeter=2*(l+b)
+print(perimeter)
