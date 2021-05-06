0c1fb9f2bfad7746bd76b26d27d45c344096b166
philomathtanya <tanyabansal525@gmail.com>
2021-05-05 22:27:13 -0700

    methods of dict

---
+++ method of dictionary.py
@@ -0,0 +1,32 @@
+dict={'name':"tanya bansal",'class':"btech cs",'section':"A",'university':"G.l.A"}
+'''1.a=dict.clear()
+print(a)
+2.a=dict.copy()
+print(a)
+v=[1]
+3.a=dict.fromkeys(dict,v)
+#it will specified same value to the keys which is given in v and when we will print only dict then it will give keys 
+its value none.
+print(a)
+4.a=dict.get('name')
+print(a)
+5.a=dict.items()
+print(a)
+6.a=dict.keys()
+print(a)
+7.a=dict.popitem()
+#it removes and gives the last key and its value and after when you will print dict it will remove last key with its
+value.
+print(a)
+print(dict)
+8.a=dict.setdefault('years',22)
+#you cane type here name it will return its value but if you have given any other key which is not given in the dict 
+then it will give none
+print(a)
+9.a=dict.pop('name')
+print(dict)
+10.a=dict.values()
+print(a)
+u={"years":'25'}
+11.a=dict.update(u)
+print(dict)'''
