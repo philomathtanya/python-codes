1d87358e6b6db5ae350d64db4dd0ddf39be1dcd6
philomathtanya <tanyabansal525@gmail.com>
2021-05-09 21:02:46 -0700

    to print dict in table format

---
+++ to print dict in table format.py
@@ -0,0 +1,19 @@
+dict={'name':"tanya bansal",'class':"btech cs",'section':"A",'university':"G.l.A",'place':"G.l.A"}
+a=dict.keys()
+b=dict.values()
+for i in a:
+    print(i,end="   ")
+print()
+for i in b:
+    print(i, end="   ")
+
+
+
+
+
+
+
+
+
+
+
